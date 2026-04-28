6a7de638f75c865c0afa1da7b543342b76bbf04f
ayush <ayushsonu821312@gmail.com>
2026-04-28 09:24:22 +0530

    making ui for home screen and adding footer

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+from src.screens.home_screen import home_screen
+from src.screens.teacher_screen import teacher_screen
+from src.screens.student_screen import student_screen
+
+def main():
+    home_screen()
+    
+
+main()
